da79344d6ac69223fc9fef4df7b8fdec95dd1cbb
Gioanna Rheumer <ratofkryll@gmail.com>
2019-01-17 23:51:09 +0000

    removed unused variable

--- app/mailers/order_confirmation_mailer.rb
+++ app/mailers/order_confirmation_mailer.rb
@@ -3,7 +3,6 @@ class OrderConfirmationMailer < ApplicationMailer
 
   def order_confirmation_email(order)
     @order = order
-    @url = "http://jungle.com"
     mail(to: @order.email, subject: "Order ##{@order.id} Confirmation")
   end
 end
