7c81d431c9c91bb3afa5464bb3cdaf77f64c1972
Gioanna Rheumer <ratofkryll@gmail.com>
2019-01-18 23:05:16 +0000

    added review posting

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,14 +1,24 @@
 class ReviewsController < ApplicationController
 
+  def new
+    @review = Review.new
+  end
+
   def create
-    @review = Reviews.new(review_params)
+    @review = Review.new(review_params)
     @review.product_id = params[:product_id]
+
+    if @review.save!
+      redirect_to product_path(@review.product_id)
+    else
+      redirect_to product_path(@review.product_id)
+    end
   end
 
   private
 
   def review_params
-    params.require(:review).permit(:rating, :description)
+    params.require(:review).permit(:rating, :description, :user_id)
   end
 
 end
