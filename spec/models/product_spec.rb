91ddd7385d4502f4ade1ae9a9f71720c903501d8
Gioanna Rheumer <ratofkryll@gmail.com>
2019-01-21 19:57:41 +0000

    added tests for product model

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    it 'is not valid without a name' do
+      category = Category.create! name: 'Category'
+      product = category.products.new({
+        name: nil,
+        quantity: 1,
+        price: 100
+        })
+      expect(product).to_not be_valid
+    end
+    it 'is not valid without a price' do
+      category = Category.create! name: 'Category'
+      product = category.products.new({
+        name: 'Product',
+        quantity: 1,
+        price: nil
+        })
+      expect(product).to_not be_valid
+    end
+    it 'is not valid without a quantity' do
+      category = Category.create! name: 'Category'
+      product = category.products.new({
+        name: 'Product',
+        quantity: nil,
+        price: 100
+        })
+      expect(product).to_not be_valid
+    end
+    it 'is not valid without a category' do
+      product = Product.new({
+        name: 'Product',
+        quantity: 1,
+        price: 100
+        })
+      expect(product).to_not be_valid
+    end
+    it 'saves errors in errors.full_messages' do
+      product = Product.create({
+        name: 'Product',
+        quantity: 1,
+        price: 100
+        })
+      expect(product.errors.full_messages).to_not be_empty
+    end
+  end
+end
