ea1f1831cd0aba97293af1f53415bc47d35c3b48
Gioanna Rheumer <ratofkryll@gmail.com>
2019-01-21 20:04:46 +0000

    changed test vars to instance vars

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,48 +3,47 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   describe 'Validations' do
     it 'is not valid without a name' do
-      category = Category.create! name: 'Category'
-      product = category.products.new({
+      @category = Category.create! name: 'Category'
+      @product = @category.products.new({
         name: nil,
         quantity: 1,
         price: 100
         })
-      expect(product).to_not be_valid
+      expect(@product).to_not be_valid
     end
     it 'is not valid without a price' do
-      category = Category.create! name: 'Category'
-      product = category.products.new({
+      @category = Category.create! name: 'Category'
+      @product = @category.products.new({
         name: 'Product',
         quantity: 1,
         price: nil
         })
-      expect(product).to_not be_valid
+      expect(@product).to_not be_valid
     end
     it 'is not valid without a quantity' do
-      category = Category.create! name: 'Category'
-      product = category.products.new({
+      @category = Category.create! name: 'Category'
+      @product = @category.products.new({
         name: 'Product',
         quantity: nil,
         price: 100
         })
-      expect(product).to_not be_valid
+      expect(@product).to_not be_valid
     end
     it 'is not valid without a category' do
-      product = Product.new({
+      @product = Product.new({
         name: 'Product',
         quantity: 1,
         price: 100
         })
-      expect(product).to_not be_valid
+      expect(@product).to_not be_valid
     end
     it 'saves errors in errors.full_messages' do
-      category = Category.create! name: 'Category'
-      product = category.products.create({
+      @product = Product.create({
         name: 'Product',
         quantity: 1,
         price: 100
         })
-      expect(product.errors.full_messages).to_not be_empty
+      expect(@product.errors.full_messages).to_not be_empty
     end
   end
 end
