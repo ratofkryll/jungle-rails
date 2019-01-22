06d967f86adde353210b7297d6d0a053650182a2
Gioanna Rheumer <ratofkryll@gmail.com>
2019-01-22 21:40:25 +0000

    added testing for homepage products

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
-RSpec.feature "HomePages", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature 'Visitor navigates to homepage', type: :feature, js: true do
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!({
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: rand(1..30),
+        price: rand(1000..5000)
+        })
+    end
+  end
+
+  scenario 'They see all products' do
+    visit root_path
+    save_screenshot
+    expect(page).to have_css 'article.product', count: 10
+  end
 end
