6466e9103f710926a96f98e832ceb0f2f3d5de43
Gioanna Rheumer <ratofkryll@gmail.com>
2019-01-22 22:08:45 +0000

    added testing for navigating to product details

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
-RSpec.feature "ProductDetails", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "ProductDetails", type: :feature, js: true do
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!({
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: rand(1..30),
+        price: rand(1000..5000)
+        })
+    end
+  end
+
+  scenario 'They click through to the product page' do
+    visit root_path
+    first('article.product').find_link('Details').click
+    expect(page).to have_css('section.products-show')
+    save_screenshot
+  end
 end
